f8280a9365a41056157d313ae71c34e80288ab22
Gini24mp <u21573558@tuks.co.za>
2023-12-06 03:42:15 +0200

    day 1 completed

--- day-1/solution.java
+++ day-1/solution.java
@@ -13,7 +13,6 @@ class solution {
 
         updateMap(map,key, list);
 
-        // processString(MutableStr, original, map);
     }
 
     public static String updateMutableStr(List<Integer> list,String MutableStr,String original, String FirstReg, String SecondReg){
@@ -22,19 +21,35 @@ class solution {
             updateList(list, MutableStr, original,reg);
             MutableStr = MutableStr.replaceFirst(reg, "");
 
-        }else if(MutableStr.matches(SecondReg)){
+        }
+        
+        if(MutableStr.matches(SecondReg)){
             String reg = SecondReg.substring(SecondReg.indexOf("(") + 1, SecondReg.indexOf(")"));
             updateList(list, MutableStr, original, reg);
             MutableStr = MutableStr.replaceFirst(reg, "");
         }
+
         return original;
     }
 
     public static void updateList(List<Integer> list,String MutableStr,String original, String reg){
         int first = original.indexOf(reg);
         int last = original.lastIndexOf(reg);
-        list.add(first);
-        list.add(last);
+
+        if(list.size() == 2){
+            if(first < list.get(0)){
+                list.set(0, first);
+            }
+
+            if(last > list.get(1)){
+                list.set(1, last);
+            }
+
+        }else{
+            list.add(first);
+            list.add(last);
+        }
+
     }
 
     public static void updateMap(Map<String, List<Integer>> map, String key, List<Integer> list){
@@ -71,78 +86,74 @@ class solution {
         if(MutableStr.matches(".*(one).*|.*(1).*")){
 
             processMatch(MutableStr, original, map, ".*(one).*|.*(1).*", "1");
-
-        }else {
-            processVisitation(VisitorCounterMap, flags, "1");
-
+            
         }
+
+        processVisitation(VisitorCounterMap, flags, "1");
         
         if(MutableStr.matches(".*(two).*|.*(2).*")){
 
             processMatch(MutableStr, original, map, ".*(two).*|.*(2).*", "2");
 
-        }else {
-            processVisitation(VisitorCounterMap, flags, "2");
-
         }
-        
+
+        processVisitation(VisitorCounterMap, flags, "2");
         
         if(MutableStr.matches(".*(three).*|.*(3).*")){
 
             processMatch(MutableStr, original, map, ".*(three).*|.*(3).*", "3");
 
-        }else {
-            processVisitation(VisitorCounterMap, flags, "3");
         }
+
+        processVisitation(VisitorCounterMap, flags, "3");
         
         if(MutableStr.matches(".*(four).*|.*(4).*")){
 
             processMatch(MutableStr, original, map, ".*(four).*|.*(4).*", "4");
 
-        }else {
-            processVisitation(VisitorCounterMap, flags, "4");
         }
+
+        processVisitation(VisitorCounterMap, flags, "4");
         
         if(MutableStr.matches(".*(five).*|.*(5).*")){
 
             processMatch(MutableStr, original, map, ".*(five).*|.*(5).*", "5");
 
-        }else {
-            processVisitation(VisitorCounterMap, flags, "5");
         }
-        
+
+        processVisitation(VisitorCounterMap, flags, "5");
+
         if(MutableStr.matches(".*(six).*|.*(6).*")){
 
             processMatch(MutableStr, original, map, ".*(six).*|.*(6).*", "6");
 
-        }else {
-            processVisitation(VisitorCounterMap, flags, "6");
         }
-        
+
+        processVisitation(VisitorCounterMap, flags, "6");
+
         if(MutableStr.matches(".*(seven).*|.*(7).*")){
 
             processMatch(MutableStr, original, map, ".*(seven).*|.*(7).*", "7");
 
-        }else {
-            processVisitation(VisitorCounterMap, flags, "7");
         }
+
+        processVisitation(VisitorCounterMap, flags, "7");
         
         if(MutableStr.matches(".*(eight).*|.*(8).*")){
 
             processMatch(MutableStr, original, map, ".*(eight).*|.*(8).*", "8");
 
-        }else {
-            processVisitation(VisitorCounterMap, flags, "8");
         }
+
+        processVisitation(VisitorCounterMap, flags, "8");
         
         if(MutableStr.matches(".*(nine).*|.*(9).*")){
 
             processMatch(MutableStr, original, map, ".*(nine).*|.*(9).*", "9");
 
-        } else {
-            processVisitation(VisitorCounterMap, flags, "9");
+        } 
 
-        }
+        processVisitation(VisitorCounterMap, flags, "9");
 
         for(int i = 0; i<flags.length; i++){
             if(flags[i] == false){
@@ -167,7 +178,7 @@ class solution {
     public static void main(String[] args) {
 
         try {
-            File myObj = new File("test.txt");
+            File myObj = new File("input.txt");
             Scanner myReader = new Scanner(myObj);
             int total = 0;
             while (myReader.hasNextLine()) {
@@ -175,9 +186,8 @@ class solution {
                 Map<String, Integer> VisitorCounterMap = new HashMap<>();
                 Boolean[] flags = new Boolean[9];
                 Arrays.fill(flags, Boolean.FALSE);
-                String data = myReader.nextLine();
+                String data = myReader.nextLine();   
                 int subTotal = processString(data, data, map, flags, VisitorCounterMap);
-                System.out.println(subTotal);
                 total += subTotal;
             }
             myReader.close();
@@ -188,35 +198,6 @@ class solution {
         }
 
     }
+    
 }
 
-// try {
-//     File myObj = new File("test.txt");
-//     Scanner myReader = new Scanner(myObj);
-//     int total = 0;
-//     while (myReader.hasNextLine()) {
-//         String data = myReader.nextLine();
-//         String[] arrOfStr = data.split("");
-//         String num = "";
-//         for (String a : arrOfStr) {
-//             try {
-//                 Integer.parseInt(a);
-//                 num += a;
-//             } catch (NumberFormatException e) {}
-//         }
-//         String out = "";
-//         if(num.length() > 0) {
-//             if(num.length()<2){
-//                 out = String.valueOf(num.charAt(0)) + String.valueOf(num.charAt(0));
-//             }else{
-//                 out = String.valueOf(num.charAt(0)) + String.valueOf(num.charAt(num.length()-1));
-//             }
-//             total += Integer.parseInt(out);
-//         }
-//     }
-//     myReader.close();
-//     System.out.println(total);
-// } catch (FileNotFoundException e) {
-//     System.out.println("File Not found.");
-//     e.printStackTrace();
-// } 
